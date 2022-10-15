823a1097f9e8388d389c0e4d2884e8c6a994f2b6
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2022-10-15 17:38:37 -0300

    .

--- SCPs/portascan.py
+++ SCPs/portascan.py
@@ -4,7 +4,11 @@ url = 'bancocn.com'
 porta = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((url, porta))
-client.send(b"hello word")
-resposta = client.recv(1024)
-print(resposta.decode())
+client.settimeout(20)
+code  = client.connect_ex((url, porta))
+
+#client.send(b"hello word")
+#resposta = client.recv(1024)
+#print(resposta.decode())
+
+print(code)
