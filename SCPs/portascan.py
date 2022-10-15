06466456d4a41d64e11c0701af279c413e657a5c
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2022-10-15 18:05:57 -0300

    .

--- SCPs/portascan.py
+++ SCPs/portascan.py
@@ -1,14 +1,20 @@
+# codigo para verificar portas abertas 
+
 import socket
 
 url = 'bancocn.com'
-porta = 80
+porta = [21,22,80,443,445,3306,25]
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.settimeout(20)
-code  = client.connect_ex((url, porta))
+for port in porta:
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(1.5)
+    code  = client.connect_ex((url, port))
+    if code == 0:
+        print(f"|{port}| aberta")
+    else:
+        print(f"|{port}| fechada")
 
 #client.send(b"hello word")
 #resposta = client.recv(1024)
 #print(resposta.decode())
-
-print(code)
